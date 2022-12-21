5d0a88ab01c8f20b99c1324d88dbc807a7b437b1
blckvia <a.vyatkinn@icloud.com>
2022-12-21 14:05:48 +0500

    done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
-import { validateIp } from './helpers';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+import { addOffset, addTileLayer, getAdress, validateIp } from './helpers';
 
 const IpInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
 
+const ipInfo = document.querySelector('#ip');
+const locationInfo = document.querySelector('#location');
+const timezoneInfo = document.querySelector('#timezone');
+const ispInfo = document.querySelector('#isp');
+import icon from '../images/icon-location.svg';
+
 btn.addEventListener('click', getData);
 IpInput.addEventListener('keydown', handleKey);
 
+const markerIcon = L.icon({
+    iconUrl: icon,
+
+    iconSize: [30, 40], // size of the icon
+    // iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+});
+
+const mapArea = document.querySelector('.map');
+const map = L.map(mapArea, {
+    center: [51.505, -0.09],
+    zoom: 13,
+    zoomControl: false,
+});
+
+addTileLayer(map);
+L.marker([51.5, -0.09], { icon: markerIcon }).addTo(map);
+
 function getData() {
     if (validateIp(IpInput.value)) {
-        fetch(
-            `https://geo.ipify.org/api/v2/country?apiKey=at_ZvVlreKQbsPMQww73QgupwfNL2UJP&ipAddress=${IpInput.value}`
-        )
-            .then((response) => response.json())
-            .then(console.log);
+        getAdress(IpInput.value).then((data) => setInfo(data));
     }
 }
 
@@ -21,3 +43,24 @@ function handleKey(event) {
         getData();
     }
 }
+
+function setInfo(mapData) {
+    console.log(mapData);
+    const { lat, lng, country, region, timezone } = mapData.location;
+
+    ipInfo.innerText = mapData.ip;
+    locationInfo.innerText = `${country} ${region}`;
+    timezoneInfo.innerText = timezone;
+    ispInfo.innerText = mapData.isp;
+
+    map.setView([lat, lng]);
+    L.marker([lat, lng], { icon: markerIcon }).addTo(map);
+
+    if (matchMedia('max-width: 1023px').matches) {
+        addOffset(map);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    getAdress('102.22.22.1').then(setInfo);
+});
